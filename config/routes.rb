6587cddf8e6416bb14397d20acf422fa91d0d157
syakeokome <meoko4635@gmail.com>
2022-01-12 15:54:16 +0000

    管理者画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :customers
+  devise_for :admins
+  
   namespace :admin do
     resources :orders, only: [:show, :update]
   end
@@ -41,7 +45,7 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
-  devise_for :customers
-  devise_for :admins
+
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
