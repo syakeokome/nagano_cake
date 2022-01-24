daef1384eea3c7257b8c41c2335414ed9abe40a1
syakeokome <meoko4635@gmail.com>
2022-01-24 16:11:21 +0000

    マイページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_16_070531) do
+ActiveRecord::Schema.define(version: 2022_01_24_160404) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
