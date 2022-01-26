13ae43d0103ed801f6cb6e93f04371f0911e4f5a
syakeokome <meoko4635@gmail.com>
2022-01-26 14:58:04 +0000

    新規登録コントローラー

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_160404) do
+ActiveRecord::Schema.define(version: 2022_01_26_121002) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_160404) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "telephone_number", null: false
-    t.boolean "is_active", null: false
+    t.boolean "is_active", default: true, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
