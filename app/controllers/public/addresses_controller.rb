1cbc142b1882644c55325431ba3e3916396ce067
syakeokome <meoko4635@gmail.com>
2022-01-29 07:42:51 +0000

    商品一覧

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,39 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
+
+  def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to public_addresses_path
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to public_addresses_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to public_addresses_path
+
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
+  end
+
 end
