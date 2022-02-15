4ae47b0088409c25c9e5b32037e8cf63c359f6fd
syakeokome <meoko4635@gmail.com>
2022-02-15 10:56:25 +0000

    エラー解決

--- app/controllers/publics/registrations_controller.rb
+++ app/controllers/publics/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Publics::RegistrationsController < Devise::RegistrationsController
-  #before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
@@ -41,9 +41,9 @@ class Publics::RegistrationsController < Devise::RegistrationsController
 
   #protected
 
-  #def configure_permitted_parameters
-    #devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :password])
-  #end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :password])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
